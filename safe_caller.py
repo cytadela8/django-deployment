ca35338309d304d37b09ac6426eb3d10ed927ef6
Artur Puzio <contact@puzio.waw.pl>
2021-01-10 00:48:01 +0100

    safe_caller: Simplified parse logic

--- safe_caller.py
+++ safe_caller.py
@@ -4,10 +4,15 @@ import caller_config
 import subprocess
 import os
 
-def sanitize_commit_info(commit: str):
+
+class InvalidCommit(Exception):
+    pass
+
+
+def sanitize_commit_info(commit: str) -> str:
     commit = commit.strip()
     if not commit.isalnum():
-        return None
+        raise InvalidCommit()
     return commit
 
 
@@ -22,31 +27,26 @@ def main():
     if 'SSH_ORIGINAL_COMMAND' in os.environ:
         print("Looking at original SSH command...", flush=True)
         args = os.environ['SSH_ORIGINAL_COMMAND'].strip().split(' ')
-    if len(args) == 0:
-        print("Missing arguments")
-        sys.exit(1)
-    elif len(args) == 1:
-        code_commit = sanitize_commit_info(args[0])
-        config_commit = sanitize_commit_info(
-            get_master_hash_of_repo(caller_config.config_repo)
-        )
-    elif len(args) == 2:
-        code_commit = sanitize_commit_info(args[0])
-        config_commit = sanitize_commit_info(args[1])
-    else:
-        print("Too many arguments")
-        sys.exit(1)
+    try:
+        if len(args) == 1:
+            code_commit = sanitize_commit_info(args[0])
+            config_commit = sanitize_commit_info(
+                get_master_hash_of_repo(caller_config.config_repo)
+            )
+        elif len(args) == 2:
+            code_commit = sanitize_commit_info(args[0])
+            config_commit = sanitize_commit_info(args[1])
+        else:
+            print("Wrong number of arguments - " + str(len(args)))
+            sys.exit(1)
 
-    if not code_commit:
-        print("Invalid code commit")
+        subprocess.call(
+            [caller_config.fab_path, "deploy", code_commit, config_commit],
+            cwd=caller_config.deployment_script_root,
+        )
+    except InvalidCommit:
+        print("Invalid commit")
         sys.exit(1)
-    if not config_commit:
-        print("Invalid config commit")
-        sys.exit(1)
-    subprocess.call(
-        [caller_config.fab_path, "deploy", code_commit, config_commit],
-        cwd=caller_config.deployment_script_root,
-    )
 
 
 if __name__ == "__main__":
